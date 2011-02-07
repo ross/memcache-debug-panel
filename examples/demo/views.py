604aa3003843192c9b71cb9d8570a86baa27c327
rm <rwmcfa1@neces.com>
2011-02-06 19:45:01 -0800

    code to swap out to pylibmc

--- examples/demo/views.py
+++ examples/demo/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render_to_response
 from django.views.decorators.cache import cache_page
 import memcache
+#import pylibmc
 
 def index(request, **kwargs):
     client = memcache.Client(['127.0.0.1:11211'], debug=False)
+    #client = pylibmc.Client(['127.0.0.1'], binary=True)
     client.get('key')
     client.get('key2')
     try:
